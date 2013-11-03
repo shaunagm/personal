5abc1e676d40b8e6fb8d9ace4a089c0459abd1a2
Shauna <shaunagm@gmail.com>
2013-11-02 21:02:34 -0400

    subbed in functions.py create_sieve function

--- project_euler/p1-10/problem_10_summation_of_primes.py
+++ project_euler/p1-10/problem_10_summation_of_primes.py
@@ -1,15 +1,13 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-sum = 0
-sieve = [0] * 2000000
+import os
+filename = '../functions.py'
+if os.path.exists(filename): 
+        execfile(filename)
 
-# Algorithm from sieve of eratosthenes (well, a less efficient version) 
-for x in range(2,len(sieve)):
-	if (sieve[x] == 0):
-		y = x
-		for y in range(x*2,len(sieve),x):
-			sieve[y] = 1
+sum = 0
+sieve = create_sieve(2000000)
 
 for z in range(2,len(sieve)):			# python is zero-indexed
 	if (sieve[z] == 0):
@@ -23,3 +21,4 @@ print sum
 # Started with a modified version from problem 7, but it's wildly inefficient at this scale.  Luckily this problem was solved a few
 # millenia ago.  Thanks, eratosthenes (and wikipedia!)
 
+# Rewritten to use function.py (view commit 31933032c96140f4093014d1f9c1d55da0442ea9 to see original version)
